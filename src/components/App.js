0ad945d60dfc638168afa19f85009379cfd7d59e
rachelrar <rachroberts95@gmail.com>
2017-09-20 14:41:29 -0400

    fixed typo with NavBar and Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
+import Navbar from './NavBar';
 import Sidebar from './Sidebar';
 import RecipeView from './RecipeView';
 import '../App.css';
