3a7cafa134af1341b3eba9115a5ca9d424eee340
Jacob <jacobfij@buffalo.edu>
2017-09-21 01:05:36 -0400

    issue-21 added grommet, began designing the mock sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
+
+import '../App.css';
+import '../../node_modules/grommet-css'
+
+
+import Navbar from './NavBar';
 import Sidebar from './Sidebar';
 import RecipeView from './RecipeView';
-import '../App.css';
+
+import GrommetApp from 'grommet/components/App';
+import Split from 'grommet/components/Split';
+import Article from 'grommet/components/Article';
+import Headline from 'grommet/components/Headline';
+import Header from 'grommet/components/Header';
+import Image from 'grommet/components/Image';
+import Section from 'grommet/components/Section';
+
+import logo from '../logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div>
-            <Sidebar />
-            <RecipeView />
-        </div>
-      </div>
+      <GrommetApp centered={false}>
+        <Header>
+          <Image size='small' src={logo} />
+          <Headline>Welcome to React with Grommet</Headline>
+        </Header>
+        <Article direction='row'>
+          <Sidebar></Sidebar>
+          <Section style={{ width: '80%' }}><RecipeView /></Section>
+        </Article>
+      </GrommetApp>
     );
   }
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 import '../App.css';
+import GrommetSidebar from 'grommet/components/Sidebar';
+import Title from 'grommet/components/Title';
+import Search from 'grommet/components/Search';
+import List from 'grommet/components/List';
+import ListItem from 'grommet/components/ListItem';
+
+import CheckBox from 'grommet/components/CheckBox';
+
 
 class Sidebar extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { 
+      library: ['ing 1', 'search 2', '3rd ingredient'],
+      inventory: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3']
+    }
+    this._select = this._select.bind(this);
+  }
+  
+  _select(suggestion, selected) {
+    console.log(`selection ${suggestion}`);
+    console.log(`selected ${selected}`);
+    if(selected) {
+      const { library, inventory} = this.state;
+      const i = library.indexOf(suggestion);
+      if (i >= 0) {
+        library.splice(i,1);
+        inventory.unshift(suggestion);
+      } 
+      this.setState({ library, inventory });
+    }
+  }
+
   render() {
+    const { library, inventory} = this.state;
     return (
-      <div>this is the sidebar</div>
+      <GrommetSidebar size='small' separator='all'>
+        <Title align='center'>Ingredients</Title>
+        <Search
+          inline={true} suggestions={library}
+          onSelect={({ suggestion }, selected) => this._select(suggestion, selected)}
+        />
+        <List>
+          {inventory.map((ing, i) => <ListItem key={i}><CheckBox/>{ing}</ListItem>)}
+        </List>
+      </GrommetSidebar>
     );
   }
 }
