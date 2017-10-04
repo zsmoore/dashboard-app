4922baec32125c3d5ecdf00a82e036e0ad2ce015
Dan Starner <starner.daniel5@gmail.com>
2017-10-04 16:25:17 -0400

    Scrapper command initialized

--- backend/food/management/commands/scrapefood.py
+++ backend/food/management/commands/scrapefood.py
@@ -4,10 +4,14 @@ class Command(BaseCommand):
     help = 'Scrapes food from Food2Fork'
 
     def add_arguments(self, parser):
-        # parser.add_argument('poll_id', nargs='+', type=int)
-        pass
+        parser.add_argument('start_index', type=int)
+        parser.add_argument('end_index', type=int)
 
     def handle(self, *args, **options):
 
+        print(options["start_index"])
+        print(options["end_index"])
+
+        
         # Scraper code
         self.stdout.write(self.style.SUCCESS('Successfully saved Recipe!'))
